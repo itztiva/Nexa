d19f9fb88e50cd1f5c507c0dacc581e9a08df764
Itztiva <tiva@itztiva.com>
2024-06-15 12:17:26 -0400

    basic stuff

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { Hono } from "hono";
+import path from 'node:path'
+import { loadRoutes } from "./utils/startup/loadRoutes";
+import { Nexa } from "./utils/handlers/errors";
+import logger from "./utils/logs/logger";
+import { cors } from "hono/cors";
 
 const app = new Hono({ strict: false });
 
 export default app
 
+app.use("*", cors());
+
+app.notFound((c) => c.json(Nexa.basic.notFound, 404));
+
 Bun.serve({
-    port: 5555,
+    port: 5353,
     fetch: app.fetch
 })
 
-app.get("/", async (c) => {
-    return c.body("hehe")
-})
+await loadRoutes(path.join(__dirname, "routes"), app);
+
+logger.backend("Nexa started on port 5353")
---
+++ src/utils/startup/loadRoutes.ts
@@ -0,0 +1,30 @@
+import { readdir } from "fs/promises";
+import { join } from "path";
+import { Hono } from "hono";
+import logger from "../logs/logger";
+
+async function loadRoute(directory: string, file: string): Promise<void> {
+    try {
+        const RouteModule = await import(join(directory, file));
+        const defaultExport = RouteModule.default;
+
+        if (defaultExport && typeof defaultExport === "function") {
+            defaultExport();
+        } else {
+            logger.error(`${file} does not export a valid route initializer`);
+        }
+    } catch (error) {
+        logger.error(`Error loading route ${file}: ${(error as Error).message}`);
+    }
+}
+
+export async function loadRoutes(directory: string, app: Hono): Promise<void> {
+    try {
+        const files = await readdir(directory);
+        const routedFiles = files.filter((name) => name.endsWith(".ts") || name.endsWith(".js"));
+
+        await Promise.all(routedFiles.map((file) => loadRoute(directory, file)));
+    } catch (error) {
+        logger.error(`Failed to load routes: ${error}`);
+    }
+}
