6af5f102081a03b4be1f85e05c0b9b7ed6e686f8
Itztiva <tiva@itztiva.com>
2024-06-15 14:13:13 -0400

    version playlists

--- src/routes/cloudstorage.ts
+++ src/routes/cloudstorage.ts
@@ -2,6 +2,7 @@ import app from "..";
 import crypto from "crypto";
 import fs from "node:fs";
 import path from "node:path";
+import getVersion from "../utils/handlers/getVersion";
 
 export default function () {
   app.get("/fortnite/api/cloudstorage/system", async (c) => {
@@ -66,15 +67,66 @@ export default function () {
     }
   });
 
-  app.get("/fortnite/api/cloudstorage/system/:file", async (c) => {
+  app.get('/fortnite/api/cloudstorage/system/:file', async (c) => {
     try {
-      const filePath = path.join(__dirname, "../../static/hotfixes", c.req.param("file"));
-      const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
-
-      return c.body(fileContent);
+      const version = getVersion(c);
+      const filePath = path.join(
+        __dirname,
+        '../../static/hotfixes',
+        c.req.param('file')
+      );
+      let fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+  
+      if (c.req.param('file') === 'DefaultGame.ini') {
+        const replacements: { [key: number]: { find: string; replace: string } } = {
+          7.30: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          7.40: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          8.51: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          9.40: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          9.41: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          10.40: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          11.30: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          12.41: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          },
+          12.61: {
+            find: '+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))',
+            replace: '+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))'
+          }
+        };
+  
+        const replacement = replacements[version.build];
+        if (replacement) {
+          fileContent = fileContent.replace(replacement.find, replacement.replace);
+        }
+      }
+  
+      return c.text(fileContent);
     } catch (err) {
-      console.error("Error fetching system file:", err);
-      return c.status(404);
+      console.error('Error fetching system file:', err);
+      return c.notFound();
     }
   });
 
