0b910055b4aaf2be125005ab8d05bd094243a33c
Itztiva <tiva@itztiva.com>
2024-06-15 13:36:08 -0400

    request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import path from 'node:path'
+import path from "node:path";
 import { loadRoutes } from "./utils/startup/loadRoutes";
 import { Nexa } from "./utils/handlers/errors";
 import logger from "./utils/logs/logger";
@@ -7,17 +7,27 @@ import { cors } from "hono/cors";
 
 const app = new Hono({ strict: false });
 
-export default app
+export default app;
 
 app.use("*", cors());
 
 app.notFound((c) => c.json(Nexa.basic.notFound, 404));
 
 Bun.serve({
-    port: 5353,
-    fetch: app.fetch
-})
+  port: 5353,
+  fetch: app.fetch,
+});
+
+app.use(async (c, next) => {
+  if (c.req.path === "/images/icons/gear.png" || c.req.path === "/favicon.ico")
+    await next();
+  else {
+    await next();
+
+    logger.backend(`${c.req.path} | ${c.req.method} | Status ${c.res.status}`);
+  }
+});
 
 await loadRoutes(path.join(__dirname, "routes"), app);
 
-logger.backend("Nexa started on port 5353")
+logger.backend("Nexa started on port 5353");
