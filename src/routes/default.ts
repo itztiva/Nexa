0af1f6b27efaa1a79a4d37bdb0fb06b126c2a751
Itztiva <tiva@itztiva.com>
2024-06-15 15:33:47 -0400

    i hope this eos shit works

--- src/routes/default.ts
+++ src/routes/default.ts
@@ -1,73 +1,85 @@
 import app from "..";
-import path from 'node:path'
-import fs from 'node:fs'
+import path from "node:path";
+import fs from "node:fs";
 
 export default function () {
-    app.post("/datarouter/api/v1/public/data", async (c) => {
-        return c.json([])
-    });
+  app.post("/datarouter/api/v1/public/data", async (c) => {
+    return c.json([]);
+  });
 
-    app.get("/account/api/public/account/*/externalAuths", async (c) => {
-        return c.json([]);
-    });
+  app.get("/account/api/public/account/*/externalAuths", async (c) => {
+    return c.json([]);
+  });
 
-    app.get("/launcher/api/public/distributionpoints", (c) => {
-        return c.json({
-            "distributions": [
-                "http://localhost:5535/",
-                "https://download.epicgames.com/",
-                "https://epicgames-download1.akamaized.net/",
-                "https://fastly-download.epicgames.com/"
-            ]
-        });
+  app.get("/launcher/api/public/distributionpoints", (c) => {
+    return c.json({
+      distributions: [
+        "http://localhost:5535/",
+        "https://download.epicgames.com/",
+        "https://epicgames-download1.akamaized.net/",
+        "https://fastly-download.epicgames.com/",
+      ],
     });
+  });
 
-    app.get("/Builds/Fortnite/Content/CloudDir/*.manifest", async (c: any) => {
-        c.header("Content-Type", "application/octet-stream");
-        const manifest: any = fs.readFileSync(path.join(__dirname, "..", "..", "static", "assets", "Moonlight.manifest"));
-        return c.body(manifest);
-    });
-    
-    app.get("/Builds/Fortnite/Content/CloudDir/*.ini", async (c: any) => {
-        const ini: any = fs.readFileSync(path.join(__dirname, "..", "..", "static", "assets", "stuff.ini"));
-        return c.body(ini);
-    });
-    
-    app.post("/fortnite/api/game/v2/grant_access/*", async (c) => {
-        c.json({});
-        return c.status(204);
-    });
+  app.post("/datarouter/api/v1/public/data/clients", async (c) => {
+    return c.json([]);
+  });
+  
+  app.post("/telemetry/data/datarouter/api/v1/public/data", async (c) => {
+    return c.json([]);
+  });
 
-    app.get("/fortnite/api/game/v2/enabled_features", async (c) => {
-        return c.json({});
-    });
+  app.get("/Builds/Fortnite/Content/CloudDir/*.manifest", async (c: any) => {
+    c.header("Content-Type", "application/octet-stream");
+    const manifest: any = fs.readFileSync(
+      path.join(__dirname, "..", "..", "static", "assets", "Moonlight.manifest")
+    );
+    return c.body(manifest);
+  });
 
-    app.post("/fortnite/api/game/v2/tryPlayOnPlatform/account/*", async (c) => {
-        c.header("Content-Type", "text/plain");
-        return c.text("true");
-    });
+  app.get("/Builds/Fortnite/Content/CloudDir/*.ini", async (c: any) => {
+    const ini: any = fs.readFileSync(
+      path.join(__dirname, "..", "..", "static", "assets", "stuff.ini")
+    );
+    return c.body(ini);
+  });
 
-    app.get("/fortnite/api/v2/versioncheck/*", async (c) => {
-        return c.json({
-            type: "NO_UPDATE",
-        });
-    });
+  app.post("/fortnite/api/game/v2/grant_access/*", async (c) => {
+    c.json({});
+    return c.status(204);
+  });
 
-    app.post("/api/v1/user/setting", async (c) => {
-        return c.json({});
-    });
+  app.get("/fortnite/api/game/v2/enabled_features", async (c) => {
+    return c.json({});
+  });
 
-    app.get("/fortnite/api/receipts/v1/account/*/receipts", async (c) => {
-        return c.json([]);
-    });
+  app.post("/fortnite/api/game/v2/tryPlayOnPlatform/account/*", async (c) => {
+    c.header("Content-Type", "text/plain");
+    return c.text("true");
+  });
 
-    app.get("/account/api/public/account/:accountId/externalAuths", async (c) => {
-        c.status(204);
-        return c.json({});
+  app.get("/fortnite/api/v2/versioncheck/*", async (c) => {
+    return c.json({
+      type: "NO_UPDATE",
     });
+  });
 
-    app.post("/fortnite/api/game/v2/tryPlayOnPlatform/account/*", async (c) => {
-        c.header("Content-Type", "text/plain");
-        return c.text("true");
-    });
+  app.post("/api/v1/user/setting", async (c) => {
+    return c.json({});
+  });
+
+  app.get("/fortnite/api/receipts/v1/account/*/receipts", async (c) => {
+    return c.json([]);
+  });
+
+  app.get("/account/api/public/account/:accountId/externalAuths", async (c) => {
+    c.status(204);
+    return c.json({});
+  });
+
+  app.post("/fortnite/api/game/v2/tryPlayOnPlatform/account/*", async (c) => {
+    c.header("Content-Type", "text/plain");
+    return c.text("true");
+  });
 }
--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -1,6 +1,7 @@
 import app from "..";
 import { Nexa } from "../utils/handlers/errors";
 import jwt from "jsonwebtoken";
+import logger from "../utils/logs/logger";
 
 interface requestBody {
   [key: string]: any;
@@ -40,6 +41,83 @@ export default function () {
       in_app_id: accountId,
       device_id: "deviceId",
     });
+  });
+
+  app.post("/epic/oauth/v2/token", async (c) => {
+    try {
+      const body: any = await c.req.parseBody()
+      const scope = c.req.param("scope")
+      const JWT = body.refresh_token.replace("eg1~", "")
+      const JWTdecode: any = jwt.decode(JWT)
+          let access_token = jwt.sign({
+        "sub": JWTdecode["sub"],
+        "pfsid": "fn",
+        "iss": "https://api.epicgames.dev/epic/oauth/v1",
+        "dn": JWTdecode["sub"],
+        "nonce": "n-01/jkXYh/9P5JimUEpSisDyK3Xw=",
+        "pfpid": "prod-fn",
+        "sec": 1,
+        "aud": "ec684b8c687f479fadea3cb2ad83f5c6",
+        "pfdid": "62a9473a2dca46b29ccf17577fcf42d7",
+        "t": "epic_id",
+        "scope": scope,
+        "appid": "fghi4567FNFBKFz3E4TROb0bmPS8h1GW",
+        "exp": 9668536326,
+        "iat": 1668529126,
+        "jti": JWTdecode["jti"]
+      }, "ciao");
+  
+          let refresh_token = jwt.sign({
+          "sub": JWTdecode["sub"],
+          "pfsid": "fn",
+          "iss": "https://api.epicgames.dev/epic/oauth/v1",
+          "dn": JWTdecode["sub"],
+          "pfpid": "prod-fn",
+          "aud": "ec684b8c687f479fadea3cb2ad83f5c6",
+          "pfdid": "62a9473a2dca46b29ccf17577fcf42d7",
+          "t": "epic_id",
+          "appid": "fghi4567FNFBKFz3E4TROb0bmPS8h1GW",
+          "scope": scope,
+          "exp": 9668557926,
+          "iat": 1668529126,
+          "jti": JWTdecode["jti"]
+          }, "ciao");
+  
+          let id_token = jwt.sign({
+          "sub": JWTdecode["sub"],
+          "pfsid": "fn",
+          "iss": "https://api.epicgames.dev/epic/oauth/v1",
+          "dn": JWTdecode["sub"],
+          "nonce": "n-e3Kcqw0hulXkbebFRBL8o5AwL3M=",
+          "pfpid": "prod-fn",
+          "aud": "ec684b8c687f479fadea3cb2ad83f5c6",
+          "pfdid": "62a9473a2dca46b29ccf17577fcf42d7",
+          "t": "id_token",
+          "appid": "fghi4567FNFBKFz3E4TROb0bmPS8h1GW",
+          "exp": 9668536326,
+          "iat": 1668529126,
+          "jti": JWTdecode["jti"]
+          }, "ciao");
+  
+      return c.json({
+        "scope":"basic_profile friends_list openid presence",
+        "token_type":"bearer",
+        "access_token": access_token,
+        "refresh_token":refresh_token,
+        "id_token":id_token,
+        "expires_in":115200,
+        "expires_at":"9999-12-31T23:59:59.999Z",
+        "refresh_expires_in":115200,
+        "refresh_expires_at":"9999-12-31T23:59:59.999Z",
+        "account_id":JWTdecode["sub"],
+        "client_id":"ec684b8c687f479fadea3cb2ad83f5c6",
+        "application_id":"fghi4567FNFBKFz3E4TROb0bmPS8h1GW",
+        "selected_account_id":JWTdecode["sub"],
+        "merged_accounts": []
+       })
+      } catch (error) {
+        logger.error(error)
+      }
   });
 
   app.get("/account/api/oauth/verify", async (c) => {
