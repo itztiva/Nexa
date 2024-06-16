cf0f9e449c47530806e8915f4b9ef7a7816df55e
Itztiva <tiva@itztiva.com>
2024-06-16 19:36:22 -0400

    store access

--- src/routes/storefront.ts
+++ src/routes/storefront.ts
@@ -6,9 +6,7 @@ import fs from "fs";
 
 export default function () {
   app.get("/fortnite/api/storefront/v2/keychain", async (c) => {
-    const keychain = await axios.get(
-      "https://api.nitestats.com/v1/epic/keychain"
-    );
+    const keychain = await axios.get("https://api.nitestats.com/v1/epic/keychain");
     return c.json(keychain.data);
   });
 
@@ -16,26 +14,30 @@ export default function () {
     return c.json([]);
   });
 
+  app.get("/fortnite/api/storeaccess/v1/request_access/:accountId", async (c) => {
+    return c.json([]);
+  });
+
   app.post("/affiliate/api/public/affiliates/slug/:affiliateName", async (c) => {
-    const affiliateName = c.req.param("affiliateName")
+    const affiliateName = c.req.param("affiliateName");
     return c.json({
       id: "aabbccddeeff11223344556677889900",
       slug: affiliateName,
       displayName: affiliateName,
       status: "ACTIVE",
-      verified: true
-    })
-  })
+      verified: true,
+    });
+  });
 
   app.get("/fortnite/api/storefront/v2/catalog", async (c) => {
     const v1 = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "../../static/shop/v1.json"), "utf8")
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v1.json"), "utf8"),
     ); // to build 26.2
     const v2 = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "../../static/shop/v2.json"), "utf8")
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v2.json"), "utf8"),
     ); // to build 30.00
     const v3 = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "../../static/shop/v3.json"), "utf8")
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v3.json"), "utf8"),
     ); // latest
 
     const ver = getVersion(c);
