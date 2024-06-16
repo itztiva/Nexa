54a14df899bb0106661ab5837afb1a0ed1b67943
Itztiva <tiva@itztiva.com>
2024-06-16 19:18:57 -0400

    catalog shared bulk offers

--- src/routes/storefront.ts
+++ src/routes/storefront.ts
@@ -12,13 +12,23 @@ export default function () {
     return c.json(keychain.data);
   });
 
+  app.get("/catalog/api/shared/bulk/offers", async (c) => {
+    return c.json([]);
+  });
+
   app.get("/fortnite/api/storefront/v2/catalog", async (c) => {
-    const v1 = JSON.parse(fs.readFileSync(path.join(__dirname, "../../static/shop/v1.json"), "utf8")); // to build 26.2
-    const v2 = JSON.parse(fs.readFileSync(path.join(__dirname, "../../static/shop/v2.json"), "utf8")); // to build 30.00
-    const v3 = JSON.parse(fs.readFileSync(path.join(__dirname, "../../static/shop/v3.json"), "utf8")); // latest
+    const v1 = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v1.json"), "utf8")
+    ); // to build 26.2
+    const v2 = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v2.json"), "utf8")
+    ); // to build 30.00
+    const v3 = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../../static/shop/v3.json"), "utf8")
+    ); // latest
 
     const ver = getVersion(c);
-    
+
     switch (true) {
       case ver.build >= 30.1:
         return c.json(v3);
@@ -27,6 +37,5 @@ export default function () {
       default:
         return c.json(v1);
     }
-
   });
 }
