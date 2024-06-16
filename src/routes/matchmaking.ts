8721d992257a1fd8c6c8c19e7743429af9988e2b
Itztiva <tiva@itztiva.com>
2024-06-16 19:09:04 -0400

    findplayer route

--- src/routes/matchmaking.ts
+++ src/routes/matchmaking.ts
@@ -4,4 +4,8 @@ export default function () {
   app.get("/waitingroom/api/waitingroom", async (c) => {
     return c.json([]);
   });
+  app.get("/fortnite/api/matchmaking/session/findPlayer/:id", async (c) => {
+    return c.json([]);
+  });
+
 }
