4f58d434630efbc0b3af1104644f970e6631c80b
Itztiva <tiva@itztiva.com>
2024-06-16 19:09:13 -0400

    session join route

--- src/routes/matchmaking.ts
+++ src/routes/matchmaking.ts
@@ -4,8 +4,12 @@ export default function () {
   app.get("/waitingroom/api/waitingroom", async (c) => {
     return c.json([]);
   });
-  app.get("/fortnite/api/matchmaking/session/findPlayer/:id", async (c) => {
+  app.get(
+    "/fortnite/api/matchmaking/session/findPlayer/:id", async (c) => {
     return c.json([]);
   });
 
+  app.post("/fortnite/api/matchmaking/session/:SessionId/join", async (c) => {
+    return c.json([]);
+  });
 }
