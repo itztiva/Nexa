067c0ab6ea5ab8119eb211b3147f70144f5c4d9a
Itztiva <tiva@itztiva.com>
2024-06-16 09:04:45 -0400

    maybe thats why clientsettings didnt load oopsie

--- src/routes/cloudstorage.ts
+++ src/routes/cloudstorage.ts
@@ -144,15 +144,6 @@ export default function () {
     }
   });
 
-  app.get("/fortnite/api/cloudstorage/user/:accountId", async (c) => {
-    try {
-      return c.json([]);
-    } catch (err) {
-      console.error("Error fetching user cloudstorage:", err);
-      return c.status(500);
-    }
-  });
-
   app.put("/fortnite/api/cloudstorage/user/:accountId/:file", async (c) => {
     const filename = c.req.param("file");
 
