789b0e2a65db728a1a80d074dc9800a28d365e0f
Itztiva <tiva@itztiva.com>
2024-06-16 22:00:06 -0400

    world info

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -48,6 +48,10 @@ export default function () {
     });
   });
 
+  app.get("/fortnite/api/game/v2/world/info", async (c) => {
+    return c.json({});
+  });
+
   app.get("/api/v2/interactions/aggregated/Fortnite/:accountId", async (c) => {
     return c.json([]);
   });
