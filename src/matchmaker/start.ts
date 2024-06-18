d6debec317b4e0cffe72eb33183b3aa8ea38572e
Itztiva <tiva@itztiva.com>
2024-06-18 00:17:13 -0400

    acc this isnt needed rn

--- src/matchmaker/start.ts
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer, WebSocket } from "ws";
-import { handleStates } from "./handlers/Message";
-
-const server = new WebSocketServer({ port: 5555 });
-
-server.on("connection", (ws: WebSocket & { dispatchEvent: (event: Event) => boolean }) => {
-  handleStates(ws);
-});
