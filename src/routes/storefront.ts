d69b625f6860a295b5933fb49b87bbede9098af5
Itztiva <tiva@Itztiva.com>
2025-01-07 19:45:45 -0500

    fixed loadouts and shi ig

--- src/routes/storefront.ts
+++ src/routes/storefront.ts
@@ -5,10 +5,11 @@ import path from "path";
 import fs from "fs";
 import { Nexa } from "../utils/handlers/errors";
 
+const keychain = await Bun.file("static/shop/keychain.json").json();
+
 export default function () {
   app.get("/fortnite/api/storefront/v2/keychain", async (c) => {
-    const keychain = await axios.get("https://api.nitestats.com/v1/epic/keychain");
-    return c.json(keychain.data);
+    return c.json(keychain);
   });
 
   app.get("/catalog/api/shared/bulk/offers", async (c) => {
